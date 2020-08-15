9edadaced3b24af313e923bddf28700f84dbb653
alejandro-mun <63469503+alejandro-mun@users.noreply.github.com>
2020-08-15 18:38:28 -0400

    Add files via upload
    Some changes need to be made such as order of images. They are backward. On some devices the octagon is too large.

---
+++ js/octagon_code.js
@@ -0,0 +1,72 @@
+// JavaScript Document
+$(document).ready(function () {
+	
+	//we add code for arrows and keyboard here!!!
+	var Yrotate = 0;	//The current degrees of myhex
+	
+	//Left arrow click
+	$('#left_arrow').click(function(){
+		Yrotate += 45;
+		doMove();
+
+
+	})
+
+	//Right Arrow click
+	
+	$('#right_arrow').click(function(){
+		Yrotate -=45;
+		doMove();	//Call the function doMove
+
+	})
+	
+	
+	
+
+	///detect swipe
+
+
+	document.onkeydown = function(event) {
+		switch (event.key){		//when a key is pressed
+			//Compare the value ofthe key taht was pressed to:
+			case 'ArrowLeft':		//If it matches arrow left
+				Yrotate +=45;		//decrease the Y by 60
+				event.preventDefault();	//prevent it from doing a default role
+				break;
+
+			case 'ArrowRight':
+				Yrotate -=45;
+				event.preventDefault();
+				break;
+		}
+		doMove();
+	}
+
+	$('#parent_of_myhex').on("swipeleft", swipeLeftHandler);
+
+	function swipeLeftHandler(event) {
+		Yrotate -= 45;
+		doMove();
+	}
+
+	$('#parent_of_myhex').on("swiperight", swipeRightHandler);
+
+	function swipeRightHandler(event) {
+		Yrotate += 45;
+		doMove();
+	}
+
+
+
+
+	//This is the function to actually spin
+	function doMove() {
+		$('#myhex').css("transform", "rotateY(" + Yrotate + "deg) ");
+	}
+
+	
+
+	
+	
+
+}); ///end document ready
